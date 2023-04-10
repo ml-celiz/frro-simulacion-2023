424ca730f09dfba11cfaa5ea18132f26aa4f73e0
ml-celiz <marialuzceliz888@gmail.com>
2023-04-10 11:10:04 -0300

    Martingala terminado

--- Trabajo práctico 1.2/TP 1.2 D'Alembert.py
+++ Trabajo práctico 1.2/TP 1.2 D'Alembert.py
@@ -3,14 +3,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-n=1 #Numero por el que apuesto
+n= 1
 contador=0
-r=6 #Corridas
+r=6
 tiradas=1000
 dineroIni=5000
 apuestaIni=150
 
-#Generar ruletas (6 corridas con 1000 tiradas c/u)
+#Generar ruletas
 ruletas=[] * r
 for j in range(r):
     ruletas.append([] * tiradas)
@@ -19,9 +19,9 @@ for j in range(r):
 
 #Generar salidas
 #Salidas es un array que simplifica el resultado de la ruleta con respecto al tipo de apuesta:
-# Si sale un cero en la ruleta, salidas tendra un 0.
-# Si sale un numero del 1 al 18, salidas tendra un 1.
-# Si sale un numero del 19 al 36, salidas tendra un 2.
+#   Si sale un cero en la ruleta, salidas tendra un 0.
+#   Si sale un numero del 1 al 18, salidas tendra un 1.
+#   Si sale un numero del 19 al 36, salidas tendra un 2.
 salidas = [] * r
 for i in range(r):
     salidas.append([] * tiradas)
@@ -38,9 +38,9 @@ for i in range(r):
 billeteras= [] * r
 for i in range(r):
     billeteras.append([] * tiradas)
-    billeteras[i].append(dineroIni) #Por cada tirada, en cada posicion de la sublista se coloca el dinero inicial que es 5k
+    billeteras[i].append(dineroIni)
 
-#Martingala
+#Generar frecuencias y apostar
 #Para cambiar entre apuestas con capital finito o infinito, se deben comentar o descomentar las lineas que aquí aclararemos.
 #Al comentar las lineas estarías en capital infinito. Al descomentarlas estarías en capital finito.
 frecuencias= [] * r
@@ -52,26 +52,25 @@ for i in range (r):
     frecuencias.append([] * tiradas)
     for j in range(1, tiradas):
         #Comentar las siguientes dos lineas.
-        if(bancarrota): #Si bancarrota=True la simulacion termina
+        if(bancarrota):
             break
         #Reemplazar con "elif" para capital finito, reemplazar con "if" para capital infinito en la siguiente linea.
-        elif (salidas[i][j]==n): #Si el numero es igual al que aposte -> "gano"
+        elif (salidas[i][j]==n):
             contador +=1
-            valor=(billeteras[i][j-1]+apuesta) #Valor = valor inicial + lo que aposte
+            valor=(billeteras[i][j-1]+apuesta)
             billeteras[i].append(valor)
-            apuesta=apuestaIni #Si gano mi apuesta sigue igual
-        else: #Si el numero no es igual al que aposte -> "pierdo"
-            valor=(billeteras[i][j-1]-apuesta) #Valor = valor inicial - lo que aposte
+            apuesta=apuestaIni
+        else:
+            valor=(billeteras[i][j-1]-apuesta)
             billeteras[i].append(valor)
-            apuesta *=2 #Si pierdo doblego mi apuesta
+            apuesta *=2
             #Comentar las siguientes dos lineas.
-            if (billeteras[i][j]<apuesta): #Si mi dinero es mejor que lo que apuesto, estoy en bancarrota
+            if (billeteras[i][j]<apuesta):
                 bancarrota=True    
-        fr=contador/(j) #Frecuencia = contador (cuantas veces "gane") / cantidad de numeros
+        fr=contador/(j)
         frecuencias[i].append(fr)
 
-#Graficas p/ 1 simulacion
-#Grafica frecuencia relativa
+#Graficas
 plt.title("Frecuencia Relativa")
 plt.xlabel("Cantidad de tiradas")
 plt.ylabel("Frecuencia")
@@ -79,7 +78,6 @@ plt.axhline(0.486, color='k',ls="dotted", xmax=tiradas)
 plt.plot(frecuencias[0],linestyle='solid')
 plt.show()
 
-#Grafica frecuencia relativa en grafico de barras
 plt.subplot(2,3,1)
 n1=plt.bar(np.arange(len(frecuencias[0])), frecuencias[0])
 plt.axhline(0.486, color='k',ls="dotted", xmax=tiradas)
@@ -87,7 +85,6 @@ plt.xlabel("Cantidad de tiradas")
 plt.ylabel("Frecuencia")
 plt.show()
 
-#Graficar dinero
 plt.title("Dinero en la Billetera")
 plt.axhline(dineroIni, color='k',ls="dotted", xmax=tiradas)
 plt.axhline(0, color='k',ls="dotted", xmax=tiradas)
@@ -96,8 +93,7 @@ plt.ylabel("Dinero")
 plt.plot(billeteras[0],linestyle='solid')
 plt.show()
 
-#Graficas p/ 6 simulaciones
-#Grafica frecuencia relativa
+#Graficar frecuencia relativa
 plt.title("Frecuencia Relativa")
 plt.xlabel("Cantidad de tiradas")
 plt.ylabel("Frecuencia")
@@ -106,7 +102,7 @@ for i in range (r):
     plt.plot(frecuencias[i],linestyle='solid')
 plt.show()
 
-#Grafica frecuencia relativa en grafico de barras
+#Graficar frecuencia relativa en código de barras
 plt.subplot(2,3,1)
 n1=plt.bar(np.arange(len(frecuencias[0])), frecuencias[0])
 plt.axhline(0.486, color='k',ls="dotted", xmax=tiradas)
